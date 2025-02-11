49407827bf03c6f0162d5ad6fc75900f5b182f53
Ntells <ntellsworth1@buffs.wtamu.edu>
2025-02-10 22:40:58 -0500

    Homeworkkkkkk REEEEEEEEEE

---
+++ Homework3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+public class HelloWorld
+{
+    public static void Main(string[] args) {
+        
+        Console.WriteLine("Input an integer:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine(isPrime(n));
+        
+        
+        Console.WriteLine("\nAssign an int value to N");
+        n = Convert.ToInt32(Console.ReadLine());
+        
+        for(int i = 1; i <= n; i++){
+            Console.WriteLine(new string('#',n));
+        }
+        
+    
+        Console.WriteLine("\nAssign an int value to N");
+        n = Convert.ToInt32(Console.ReadLine());
+        
+        for(int i = 1; i <= n; i++){
+            Console.WriteLine(new string('*',i));
+        }
+    }
+    
+    
+    
+    
+    static bool isPrime(int n) {
+        if(n == 1){
+            Console.WriteLine("N is non-prime.");
+            return false;
+        }
+        if(n == 2 || n == 3){
+            Console.WriteLine("N is prime.");
+            return true;
+        }
+        if(n % 2 == 0){
+            Console.WriteLine("N is non-prime.");
+            return false;
+        }
+        
+        for(int i = 2; i < n; i++){
+            if(n % i == 0){
+                Console.WriteLine("N is non-prime.");
+                return false;
+            }
+        }
+        Console.WriteLine("N is prime.");
+        return true;
+    }
+}
